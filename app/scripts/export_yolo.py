19b78e72120f9d09f6c945b3dd803691d6496dfd
&mazen <mazenkhairy200@gmail.com>
2026-04-16 05:21:25 +0200

    delete some changes

--- app/scripts/export_yolo.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load the lightweight Nano segmentation model
-model = YOLO('yolov8n-seg.pt')
-
-# Export the model to ONNX format
-model.export(format='onnx', dynamic=True)
-
-print("YOLOv8-seg exported successfully!")
