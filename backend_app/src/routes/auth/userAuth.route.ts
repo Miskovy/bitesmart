c2d6973a9fd5a1fad5cff5cfacbc67a8e4cffe2b
mohamedyasser8118 <144835892+mohamedyasser8118@users.noreply.github.com>
2026-05-05 17:37:39 +0000

    Promote backend into backend_app

--- backend_app/src/routes/auth/userAuth.route.ts
+++ backend_app/src/routes/auth/userAuth.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { userLogin } from "../../controllers/auth/userAuth.controller";
+import { signup } from "../../controllers/auth/userAuth.controller";
+
 const router = Router();
 
 router.post("/", catchAsync(userLogin));
-
+router.post("/", catchAsync(signup));
 
 export default router;
