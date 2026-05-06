5d9881e2c1a2e834e324c02c6bfdaacc5c3c1f71
Mazen Khairy <mazenkhairy200@gmail.com>
2026-05-06 03:51:30 +0300

    Promote backend into backend_app (#31)

--- backend_app/src/routes/index.ts
+++ backend_app/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import foodRoute from "./food/food.route";
 import authRoute from "./auth/userAuth.route";
+import predictionRoute from "./food/prediction.route";
 
 const router = Router();
 
 router.use("/login", authRoute);
 router.use("/food", foodRoute);
-
+router.use("/prediction", predictionRoute);
 export default router;
---
+++ backend_app/src/services/user/profile.service.ts
@@ -0,0 +1,10 @@
+import { userModes } from "../../types/interfaces";
+
+// TODO: Complete the profile services
+
+export const getMyProfile = async (userId: string) => {
+};
+// Be Sure to make an interface for the data , and remove 'any'
+export const updatemyProfile = async (userId: string, data: any) => {};
+
+export const enableMode = async (mode: userModes) => {};
