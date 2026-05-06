0aa3215ca0715c2f2b440212d24d7864705b015c
Mazen Khairy <mazenkhairy200@gmail.com>
2026-05-06 23:46:08 +0300

    Promote backend into backend_app (#33)

--- backend_app/src/routes/index.ts
+++ backend_app/src/routes/index.ts
@@ -2,10 +2,16 @@ import { Router } from "express";
 import foodRoute from "./food/food.route";
 import authRoute from "./auth/userAuth.route";
 import predictionRoute from "./food/prediction.route";
+import profileRoute from "./user/profile.route";
+import coachRoute from "./coach/coach.route"
+import { authenticated } from "../middlewares/Authenticated";
 
 const router = Router();
 
-router.use("/login", authRoute);
 router.use("/food", foodRoute);
+router.use("/login", authRoute);
 router.use("/prediction", predictionRoute);
+router.use(authenticated);
+router.use("/profile", profileRoute);
+router.use("/coach", coachRoute);
 export default router;
--- backend_app/src/types/interfaces.ts
+++ backend_app/src/types/interfaces.ts
@@ -3,3 +3,47 @@ export interface userModes {
     ramadanMode?: boolean;
 };
 
+export interface UpdateProfileData {
+    name?: string;
+    avatar?: string;
+    height?: number;
+    weight?: number;
+    BMI?: number;
+    gender?: "Male" | "Female";
+    age?: number;
+    activityLevel?: "Sedentary" | "LightlyActive" | "ModeratelyActive" | "VeryActive";
+    userGoal?: "WeightLoss" | "Maintenance" | "MuscleGain";
+
+    medicalConditions?: {
+        isDiabetesType1?: boolean;
+        isDiabetesType2?: boolean;
+        isHypertension?: boolean;
+        isPCOS?: boolean;
+        isAnemia?: boolean;
+        isCeliacDisease?: boolean;
+        isIBS?: boolean;
+    };
+
+    dietaryPreferences?: {
+        isVegetarian?: boolean;
+        isVegan?: boolean;
+        isKeto?: boolean;
+        isPaleo?: boolean;
+        isGlutenFree?: boolean;
+        isHalal?: boolean;
+        isPescatarian?: boolean;
+        isGlp1User?: boolean;
+    };
+
+    targets?: {
+        calTotal?: number;
+        proteins?: number;
+        carbs?: number;
+        fats?: number;
+        iron_mg?: number;
+        sodium_mg?: number;
+        vitamin_d_iu?: number;
+        water_ml?: number;
+    };
+}
+
