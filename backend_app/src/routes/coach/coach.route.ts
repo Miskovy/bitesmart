628e3d6577a6b10bf019c03c791bec3f522067cc
Mazen Khairy <mazenkhairy200@gmail.com>
2026-05-13 05:17:49 +0300

    Promote backend into backend_app (#37)

--- backend_app/src/routes/coach/coach.route.ts
+++ backend_app/src/routes/coach/coach.route.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
-
+import { getAllUserChats, getUserChatMessages, deleteUserChat, sendUserMessage, sendUserMessageStream } from "../../controllers/user/coach.controller";
+import catchAsync from "../../utils/catchAsync";
 
 const router = Router();
 
+router.get("/sessions", catchAsync(getAllUserChats));
+router.get("/sessions/:chatId/history", catchAsync(getUserChatMessages));
+router.delete("/sessions/:chatId", catchAsync(deleteUserChat));
+
+router.post("/chat", catchAsync(sendUserMessage));
+router.post("/chat/stream", catchAsync(sendUserMessageStream));
 
 export default router;
