c16326f9a362a4b1acf9f06ed503bd610cdef755
Mazen Khairy <mazenkhairy200@gmail.com>
2026-05-15 15:35:13 +0300

    Promote flutter into flutter_app (#52)
    Co-authored-by: MoazDief <233620225+MoazDief@users.noreply.github.com>

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,61 +2,34 @@ import 'package:bite_smart/ui/screens/splashScreen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await EasyLocalization.ensureInitialized();
-
-//   runApp(
-//     EasyLocalization(
-//       supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
-//       path: 'lang', // <-- change the path of the translation files
-//       fallbackLocale: Locale('en', 'US'),
-//       child: MyApp(),
-//     ),
-//   );
-// }
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized(); // مهم جداً
+  await EasyLocalization.ensureInitialized();
+
+// RUN APP
+
 
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
-      path: 'lang', 
+      path: 'lang', // <-- change the path of the translation files
       fallbackLocale: Locale('en', 'US'),
       child: MyApp(),
     ),
   );
 }
 
-
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       localizationsDelegates: context.localizationDelegates,
-//       supportedLocales: context.supportedLocales,
-//       locale: context.locale,
-//       debugShowCheckedModeBanner: false,
-//       home: const Splashscreen(),
-//     );
-//   }
-// }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // هذه القيم تأخذ بياناتها من EasyLocalization الموجود فوق MyApp
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
       debugShowCheckedModeBanner: false,
-      home: Splashscreen(),
+      home: const Splashscreen(),
     );
   }
 }
