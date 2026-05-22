fc9b7e1b5382bb29b944c27fb16fb3d163ccc3a1
&mazen <mazenkhairy200@gmail.com>
2026-05-22 08:05:34 +0300

    cleaning some test files

--- test_db_user.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './src/db/connection';
-import { users } from './src/models/user';
-async function test() {
-  const u = await db.query.users.findFirst();
-  console.log(u?.id);
-  process.exit(0);
-}
-test();
