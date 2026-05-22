da6a70db530392117d94ec25fc482b6525e87d6e
Mazen Khairy <mazenkhairy200@gmail.com>
2026-05-22 08:06:44 +0300

    Promote backend into backend_app (#60)

--- backend_app/test_db_user.js
+++
@@ -1,8 +0,0 @@
-const { db } = require('./src/db/connection.js');
-const { users } = require('./src/models/user.js');
-async function test() {
-  const u = await db.query.users.findFirst();
-  console.log(u.id);
-  process.exit(0);
-}
-test();
