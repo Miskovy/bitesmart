623033d6581c1ff0be424561c6a3aed53635e0fb
MoazDief <233620225+MoazDief@users.noreply.github.com>
2026-05-30 09:55:08 +0000

    Promote flutter into flutter_app

--- flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_webview_window
 import file_selector_macos
 import flutter_secure_storage_darwin
+import flutter_web_auth_2
 import google_sign_in_ios
 import shared_preferences_foundation
+import url_launcher_macos
+import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
