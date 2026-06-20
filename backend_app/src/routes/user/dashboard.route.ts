04861055de0ecba41d665f7d055b4898e4e16ec8
Miskovy <33718261+Miskovy@users.noreply.github.com>
2026-06-20 20:56:45 +0000

    Promote backend into backend_app

--- backend_app/src/routes/user/dashboard.route.ts
+++ backend_app/src/routes/user/dashboard.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { getDashboardController } from "../../controllers/user/dashboard.controller";
+import { isAdmin } from "../../middlewares/isAdmin";
 
 const router = Router();
 
-router.get("/", catchAsync(getDashboardController));
+router.get("/", isAdmin, catchAsync(getDashboardController));
 
 export default router;
